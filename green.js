4396e55189b56b5d570a1e4cfe8a3e4bb8332fe9
Rogerio <rogerio.gordo@aparecida.go.gov.br>
2021-12-06 13:54:51 -0300

    script de consumo da API e criação da listagem de itens

--- green.js
+++ green.js
@@ -1,20 +1,10 @@
 // url para consumo do json
-const url =
-  "https://front-br-challenges.web.app/api/v2/green-thumb/?sun=high&water=regularly&pets=false";
+const url = "https://front-br-challenges.web.app/api/v2/green-thumb/?sun=high&water=regularly&pets=false";
 
 // captando os valores dos filtros
 var sun = document.getElementById("sun");
 var water = document.getElementById("water");
 var pets = document.getElementById("pets");
-
-// funcao para criar box green
-function boxGreen() {
-  var divBox = document.createElement("div");
-  var txtBox = document.createTextNode("Novo elemento!");
-  divBox.appendChild(txtBox);
-  var divResultado = document.getElementById("resultado");
-  document.body.insertBefore(divBox, divResultado);
-}
 
 // função para gerar o resultado
 function getGreen() {
@@ -23,17 +13,15 @@ function getGreen() {
     .then((response) => {
       const data = response.data;
 
+      // aguardando as selects da página
       addEventListener("change", function () {
+        // pegando os valores dos selects
         var vSun = sun.value;
         var vWater = water.value;
         var vPets = pets.value;
-        // console.log(vSun)
-        // console.log(vWater)
-        // console.log(vPets)
-        // const filtro = data.filter(item => item.sun == vSun & item.water == vWater)
 
+        // filtrando os valores
         const filtro = data.filter(function (item) {
-          //  console.log(item);
           if (item.sun == vSun && item.water == vWater) {
             return true;
           } else {
@@ -41,71 +29,36 @@ function getGreen() {
           }
         });
 
+        // percorrendo os valores do filtro
         const res = filtro.map(function (green) {
-          //  console.log(green);
-          // resultado.textContent = green.name
-
-          //  var divResultado = document.getElementById("resultado");
-          //  var titulo = document.createElement("h2");
-          //  var texto = document.createTextNode(green.name);
-          //  titulo.appendChild(texto);
-          //  divResultado.appendChild(titulo);
-
-          //  var fav = document.createElement('div');
-          //  fav.classList.add('favorite');
-          //  var txtFav = document.createTextNode("Staff favorite");
-          //  fav.appendChild(txtFav);
-          //  box.appendChild(fav);
-
-          /*
-				id: 5
-				name: "Bamboo"
-				price: 15
-				staff_favorite: false
-				sun: "low"
-				toxicity: false
-				url: "https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/lucky-bamboo.png"
-				water: "regularly"
-
-
-
-
-				 <div class="box">
-					<img src="images/plants/calathea-triostar.jpg" alt="" />
-					<h3>Bunny ears cacti</h3>
-					<div class="info">
-						<h4>$25</h4>
-						<div>
-						<img src="images/icons/pet.svg" alt="image" class="icones" />
-						<img src="images/icons/low-sun.svg" alt="image" class="icones" />
-						<img src="images/icons/1-drop.svg" alt="image" class="icones" />
-						</div>
-					</div>
-				</div>
-			*/
-
+          // captando a grid para exibição
           var divGrid = document.getElementsByClassName("grid");
+
+          // criando uma nova div e setando sua classe
           var box = document.createElement("div");
           box.classList.add("box");
-
+          
+					// exibindo a imagem da planta
           var image = '<img src="' + green.url + '" alt="images" class="icones" />';
-
-
+          
+					// exibindo o nome da planta estilizada
           var titulo = document.createElement("h3");
           var nome = document.createTextNode(green.name);
           titulo.appendChild(nome);
-
+          
+					// captando o favorito
           var fav = document.createTextNode(green.staff_favorite);
-
+          
+					// exibindo o preço estiizado
           var preco = document.createElement("h4");
           var valor = document.createTextNode(green.price);
           preco.appendChild(valor);
-
+					
+					// exibindo os ícones
           var icons = document.createElement("div");
           var icoSun = '<img src="images/icons/low-sun.svg" alt="image" class="icones" />';
           var icoPet = '<img src="images/icons/pet.svg" alt="image" class="icones" />';
           var icoWater = '<img src="images/icons/1-drop.svg" alt="image" class="icones" />';
-
         });
       });
     })
